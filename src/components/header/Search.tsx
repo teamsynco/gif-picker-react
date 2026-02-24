a958b8bc4a166e6ae29d48b865e1c9749c4c28a6
John Bonds <johnbonds@hotmail.com>
2026-02-24 14:49:08 -0800

    updates

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -3,6 +3,7 @@ import PickerContext from '../../context/PickerContext';
 import SettingsContext from '../../context/SettingsContext';
 import ClearButton from './ClearButton';
 import './Search.css';
+import PoweredByKlipy from '../header/svg/PoweredByKlipy.svg';
 
 function Search(): JSX.Element {
 	const [pickerContext, setPickerContext] = useContext(PickerContext);
@@ -21,21 +22,29 @@ function Search(): JSX.Element {
 	}
 
 	return (
-		<div className="gpr-search-container">
-			<input
-				autoFocus={settings.autoFocusSearch}
-				aria-label={'Search Tenor'}
-				placeholder={'Search Tenor'}
-				className="gpr-search"
-				data-testid="gpr-search-input"
-				type="text"
-				dir="auto"
-				maxLength={500}
-				value={pickerContext.searchTerm}
-				onChange={onChange}
-			/>
-			<div className="gpr-icn-search" />
-			{pickerContext.searchTerm.length > 0 && <ClearButton onClick={onClear} />}
+		<div>
+			<div className="gpr-search-container">
+				<input
+					autoFocus={settings.autoFocusSearch}
+					aria-label={'Search KLIPY'}
+					placeholder={'Search KLIPY'}
+					className="gpr-search"
+					data-testid="gpr-search-input"
+					type="text"
+					dir="auto"
+					maxLength={500}
+					value={pickerContext.searchTerm}
+					onChange={onChange}
+				/>
+				<div className="gpr-icn-search" />
+				{pickerContext.searchTerm.length > 0 && <ClearButton onClick={onClear} />}
+			</div>
+			<div className="gpr-powered-by">
+				<img
+					src="/components/icons/klipy-watermark.png"
+					alt="Powered by Klipy"
+				/>
+			</div>
 		</div>
 	);
 }
